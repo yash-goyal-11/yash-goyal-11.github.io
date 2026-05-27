e3d3e10d4f1ec09e87cdb9dadc469c3cdf6145d0
yash1120 <21f1001138@ds.study.iitm.ac.in>
2026-05-27 16:35:25 +1000

    Updates

--- assets/SlimeCanvas-DUMJuELd.js
+++ assets/SlimeCanvas-DUMJuELd.js
@@ -1,4 +1,4 @@
-import{j as t,C as i,r as v,V as l,a,D as x}from"./index-Ci_sO4I1.js";const d=`
+import{j as t,C as i,r as v,V as l,a,D as x}from"./index-CIRrkZiA.js";const d=`
   varying vec2 vUv;
   varying vec3 vPosition;
   void main() {
